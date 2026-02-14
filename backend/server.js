19bedb66b04f3dfa6463b1f87aed093cd7490632
waheeddesiremistaheen-spec <waheeddesiremistaheen@gmail.com>
2026-02-14 05:28:51 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,8 @@ app.get("/reservations", (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
